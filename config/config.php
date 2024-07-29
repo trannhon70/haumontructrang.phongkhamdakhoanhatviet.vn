412af6d0a02dbcbddba1c213b1a96187de10f281
trannhon70 <kevintran351996@gmail.com>
2024-07-29 09:08:59 +0700

    update config

--- config/config.php
+++ config/config.php
@@ -2,7 +2,7 @@
 define("DB_HOST", "localhost");
 define("DB_USER", "root");
 define("DB_PASS", "");
-define("DB_NAME", "phongkhamdakhanhatviet.vn");
+define("DB_NAME", "phongkhamdakhoanhatviet.vn");
 
 
 // define("DB_HOST", "localhost");
